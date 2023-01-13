bb68765b55a24d2c37145ceb0d7f52ca2b89e50f
Brian De Sousa <briandesousa1@gmail.com>
2023-01-13 15:07:29 -0500

    add markdown-based blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,19 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "briandesousa.github.io",
-    siteUrl: "https://www.briandesousa.github.io",
+    title: `briandesousa.github.io`,
+    siteUrl: `https://www.briandesousa.github.io`,
     description: `GitHub.io hosted blog built with Gatsby 5`
   },
+  trailingSlash: `always`,
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content/`
+      }
+    },
+    `gatsby-transformer-remark`
   ],
 };
---
+++ src/content/post-1.md
@@ -0,0 +1,15 @@
+---
+slug: "first-blog-post"
+date: "2023-01-05"
+title: "My First Blog Post"
+---
+
+# My First Blog Post
+
+Yadda yadda yadda
+
+Some code...
+
+```bash
+ls -halt
+```
